93d97bde970b82e246e4f018cfab6378e904602b
Ivan Grinberg <ivangrinberg@MacBook-Air-de-Ivan.local>
2026-07-22 08:36:59 -0300

    fix back_urls mercadopago

--- api/crear-pago.js
+++ api/crear-pago.js
@@ -22,7 +22,7 @@ export default async function handler(req, res) {
         description: 'Desbloqueá todas las coincidencias y el segundo bloque de preguntas',
         quantity: 1,
         currency_id: 'ARS',
-        unit_price: 4990,
+        unit_price: 100,
       },
     ],
     external_reference: sessionId,
